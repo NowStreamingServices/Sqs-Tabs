e1163795cba7f3048addc4dcdbac0e4e396527c5
NathanPaynter <nathanelpaynter@gmail.com>
2017-04-04 13:00:37 -0600

    Added underlined tabs option

--- scripts/custom.js
+++ scripts/custom.js
@@ -17,5 +17,20 @@ $(document).ready(function() {
     return false;
   });
   
+  //OPTIONAL: For underlined tabs navigation
+  //When page loads...
+  $(".tabContent").hide(); //Hide all content
+  $("ul.tabs li:first").addClass("active").show(); //Activate first tab
+  $(".tabContent:first").show(); //Show first tab content
+  //On Click Event
+
+  $("ul.tabs li").click(function() {
+    $("ul.tabs li").removeClass("active"); //Remove any "active" class
+    $(this).addClass("active"); //Add "active" class to selected tab
+    $(".tabContent").hide(); //Hide all tab content
+    var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+    $(activeTab).fadeIn(); //Fade in the active ID content
+    return false;
+  });
 });
 
